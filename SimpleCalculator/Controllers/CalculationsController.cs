f6824c6c522311a537d68a3e6716335f66977002
Chris Andrews <chrisandrews0094@gmail.com>
2018-01-03 11:28:42 -0500

    Added working calculator MV&C along with EF6.2 install

---
+++ SimpleCalculator/Controllers/CalculationsController.cs
@@ -0,0 +1,115 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SimpleCalculator.Models;
+
+namespace SimpleCalculator.Controllers
+{
+    public class CalculationsController : Controller
+    {
+        private CalculationDBContext db = new CalculationDBContext();
+
+        // GET: Calculations
+        public ActionResult Index()
+        {
+            var calcs = db.Calculations.ToList();
+            calcs.Reverse();
+            ViewBag.hist = calcs;
+            return View();
+        }
+
+        // POST: Calculations/Add
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add([Bind(Include = "ID,Param1,Param2")] Calculation calculation)
+        {
+            if (ModelState.IsValid)
+            {
+                calculation.Operator = "+";
+                calculation.Result = calculation.Param1 + calculation.Param2;
+                if (Create(calculation) == true)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View("Index", calculation);
+        }
+
+        // POST: Calculations/Subtract
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Subtract([Bind(Include = "ID,Param1,Param2")] Calculation calculation)
+        {
+            if (ModelState.IsValid)
+            {
+                calculation.Operator = "-";
+                calculation.Result = calculation.Param1 - calculation.Param2;
+                if (Create(calculation) == true)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View("Index", calculation);
+        }
+
+        // POST: Calculations/Multiply
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Multiply([Bind(Include = "ID,Param1,Param2")] Calculation calculation)
+        {
+            if (ModelState.IsValid)
+            {
+                calculation.Operator = "*";
+                calculation.Result = calculation.Param1 * calculation.Param2;
+                if (Create(calculation) == true)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View("Index", calculation);
+        }
+
+        // POST: Calculations/Divide
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Divide([Bind(Include = "ID,Param1,Param2")] Calculation calculation)
+        {
+            if (ModelState.IsValid)
+            {
+                calculation.Operator = "/";
+                calculation.Result = calculation.Param1 / calculation.Param2;
+                if (Create(calculation) == true)
+                {
+                    return RedirectToAction("Index");
+                }
+
+            }
+
+            return View("Index", calculation);
+        }
+
+        // Creates history record of calculation and returns true if successful
+        private bool Create([Bind(Include = "ID,Param1,Param2,Operator,Result")] Calculation calculation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Calculations.Add(calculation);
+                db.SaveChanges();
+                return true;
+            }
+
+            return false;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SimpleCalculator/Models/Calculation.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCalculator.Models
+{
+    public class Calculation
+    {
+            public int ID { get; set; }
+            [Display(Name = "First Number")]
+            public double Param1 { get; set; }
+            [Display(Name = "Second Number")]
+            public double Param2 { get; set; }
+            [StringLength(1)]
+            public string Operator { get; set; }
+            public double? Result { get; set; }
+
+    }
+
+    public class CalculationDBContext : DbContext
+    {
+        public DbSet<Calculation> Calculations { get; set; }
+    }
+}
