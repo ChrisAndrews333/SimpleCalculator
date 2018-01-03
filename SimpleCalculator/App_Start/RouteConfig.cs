20adcc5655b6831681a751d782db003547131972
Chris Andrews <chrisandrews0094@gmail.com>
2018-01-03 11:29:05 -0500

    Updated default route to point to Calculations index page

--- SimpleCalculator/App_Start/RouteConfig.cs
+++ SimpleCalculator/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace SimpleCalculator
@@ -16,7 +12,7 @@ namespace SimpleCalculator
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Calculations", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
